e4457350ccc6c005ae563fc30e21aa528fab1119
jar <Jarafan3@gmail.com>
2026-09-19 16:45:56 +0300

    copy past lec

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <pthread.h>
+#include <iostream>
+
+void * sample(void * data)
+{
+  auto msg = static_cast< const char* >(data);
+  size_t len = std::strlen(msg);
+  std::cout << msg << "\n";
+  return reinterpret_cast< void* >(len);
+}
+
+int main()
+{
+  char msg[] = "user data";
+  pthread_t th[1] = {};
+  int err = pthread_create(th, nullptr, sample, msg);
+  if (err)
+  {
+    std::cerr << strerror(err) << "\n";
+  }
+  size_t code[1] = {};
+  err = pthread_join(*th, reinterpret_cast< void** >(&code));
+  if (err)
+  {
+    std::cerr << strerror(err) << "\n";
+  }
+  std::cout << code[0] << "\n";
+}
